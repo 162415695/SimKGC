5c9f2ef4df6e2ab94b86b3eb49fd6c56a656bfaa
USER541944 <741205364@qq.com>
2024-09-30 12:25:21 +0800

    添加参数false初始值,重新添加use_amp参数作为命令行参数

--- config.py
+++ config.py
@@ -78,7 +78,7 @@ parser.add_argument('--neighbor-weight', default=0.0, type=float,
                     help='weight for re-ranking entities')
 parser.add_argument('--eval-model-path', default='', type=str, metavar='N',
                     help='path to model, only used for evaluation')
-parser.add_argument('--add-extra-batch', action='store_true',
+parser.add_argument('--add-extra-batch', action='store_true',default=False,
                     help='add predict tail entity or not')
 parser.add_argument('--extra-batch-limit', default=0, type=int,
                     help='-1 means unlimited upper limit of tail entity,'
--- scripts/train_wn_new.sh
+++ scripts/train_wn_new.sh
@@ -24,12 +24,13 @@ python3 -u main.py \
 --train-path "${DATA_DIR}/train.txt.json" \
 --valid-path "${DATA_DIR}/valid.txt.json" \
 --task ${TASK} \
---batch-size 512 \
+--batch-size 1024 \
 --print-freq 20 \
 --additive-margin 0.02 \
 --use-self-negative \
 --pre-batch 0 \
 --finetune-t \
+--use-amp \
 --epochs 50 \
 --workers 4 \
 --max-to-keep 5 \
