d3d0db997a40550a5fb1dc622b4d3eb1e907745f
USER541944 <741205364@qq.com>
2024-11-20 15:29:03 +0800

    去除了多跳图谱构建

--- main.py
+++ main.py
@@ -5,14 +5,12 @@ import torch.backends.cudnn as cudnn
 from config import args
 from trainer import Trainer
 from logger_config import logger
-from hop_graph import graph_build
 
 def main():
     ngpus_per_node = torch.cuda.device_count()
     cudnn.benchmark = True
     logger.info("Use {} gpus for training".format(ngpus_per_node))
     trainer = Trainer(args, ngpus_per_node=ngpus_per_node)
-    graph_build()
     logger.info('Args={}'.format(json.dumps(args.__dict__, ensure_ascii=False, indent=4)))
     trainer.train_loop()
 
