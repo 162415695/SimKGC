7052a9d12ca6f51a0f1848ac137c09a33ff3c5aa
fish <741205364@qq.com>
2025-03-01 14:43:33 +0800

    格式修改

--- logger_config.py
+++ logger_config.py
@@ -1,15 +1,24 @@
+import os
+from config import args
 import logging
 
 
 def _setup_logger():
-    log_format = logging.Formatter("[%(asctime)s %(levelname)s] %(message)s")
     logger = logging.getLogger()
     logger.setLevel(logging.INFO)
+    formatter = logging.Formatter("[%(asctime)s %(levelname)s] %(message)s")
 
-    console_handler = logging.StreamHandler()
-    console_handler.setFormatter(log_format)
-    logger.handlers = [console_handler]
+    stdout_handler = logging.StreamHandler()
+    # stdout_handler.setLevel(logging.DEBUG)
+    stdout_handler.setFormatter(formatter)
 
+    file_fpath = os.path.join(args.model_dir, 'logs.log')
+    file_handler = logging.FileHandler(file_fpath)
+    # file_handler.setLevel(logging.DEBUG)
+    file_handler.setFormatter(formatter)
+
+    logger.addHandler(file_handler)
+    logger.addHandler(stdout_handler)
     return logger
 
 
